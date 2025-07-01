861f194ec3c6f8aaf49b185111db14c74b709738
Rupam Biswas <rupam.biswas@hometriangle.com>
2025-07-01 16:03:36 +0530

    Spring security done!

--- spring-sec-demo/src/main/java/com/biswas/spring_sec_demo/controller/UserController.java
+++ spring-sec-demo/src/main/java/com/biswas/spring_sec_demo/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.biswas.spring_sec_demo.controller;
 
 import com.biswas.spring_sec_demo.model.User;
+import com.biswas.spring_sec_demo.service.JwtService;
 import com.biswas.spring_sec_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +18,27 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtService jwtService;
+
     @PostMapping("register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(user.getUserName());
+        }
+        else
+            return "Login failed";
     }
 
 }
